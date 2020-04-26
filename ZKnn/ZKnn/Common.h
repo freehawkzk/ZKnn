8a2be3e944e40ee5365cb19183f3fd1725cf0156
freehawkzk <freehawkzk@126.com>
2020-04-26 20:41:59 +0800

    remove ZKnn C++ project

--- ZKnn/ZKnn/Common.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-typedef double(*ActivationFunc)(double);
-typedef double(*ActivationPrimeFunc)(double);
-
-double Func_sigmoid(double);
-double Func_sigmoid_prime(double);
-float GetRandValue();
